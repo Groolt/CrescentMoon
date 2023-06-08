f8dc9e8568e65fca2f7529d950309718fd675dff
bahao <bahaodaoai@gmail.com>
2023-06-08 17:31:21 +0700

    QL Voucher: Done

--- app/src/main/java/com/example/damb_qlnh/activity/QLVoucherActivity.java
+++ app/src/main/java/com/example/damb_qlnh/activity/QLVoucherActivity.java
@@ -3,31 +3,81 @@ package com.example.damb_qlnh.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.damb_qlnh.adapter.VoucherAdapter;
 import com.example.damb_qlnh.R;
 import com.example.damb_qlnh.models.vouCher;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class QLVoucherActivity extends AppCompatActivity {
-
+    FirebaseFirestore db;
+    final Calendar myCalendar = Calendar.getInstance();
+    ArrayList<vouCher> ListVoucherDDR;
+    ArrayList<vouCher> ListVoucherSDR;
+    VoucherAdapter adapter;
+    SimpleDateFormat formatDay = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qlvoucher);
+        db = FirebaseFirestore.getInstance();
+
+        //switch tab
+        CardView tabDDR = findViewById(R.id.materialCardView);
+        CardView tabSDR = findViewById(R.id.cardView3);
+        tabDDR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tabDDR.getBackgroundTintList() ==
+                        ColorStateList.valueOf(ContextCompat.getColor(QLVoucherActivity.this, R.color.white)))
+                {
+                    tabDDR.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#fad745")));
+                    tabSDR.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                    renderList();
+                    adapter.setList(ListVoucherDDR);
+                }
+            }
+        });
+
+        tabSDR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tabSDR.getBackgroundTintList() ==
+                        ColorStateList.valueOf(ContextCompat.getColor(QLVoucherActivity.this, R.color.white)))
+                {
+                    tabSDR.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#fad745")));
+                    tabDDR.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                    renderList();
+                    adapter.setList(ListVoucherSDR);
+                }
+            }
+        });
+
 
         //setup action bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -36,8 +86,12 @@ public class QLVoucherActivity extends AppCompatActivity {
         toolbar.setNavigationIcon(R.drawable.round_arrow_back_24);
 
         //list voucher
+        ListVoucherDDR = new ArrayList<>();
+        ListVoucherSDR = new ArrayList<>();
+        adapter = new VoucherAdapter(this, R.layout.item_voucher, ListVoucherDDR);
         ListView lv = findViewById(R.id.lv);
-        lv.setAdapter(new VoucherAdapter(this, R.layout.item_voucher, getListVoucher()));
+        lv.setAdapter(adapter);
+        renderList();
 
         //them voucher
         ImageView addbtn = findViewById(R.id.addbtn);
@@ -51,6 +105,34 @@ public class QLVoucherActivity extends AppCompatActivity {
                 if (window == null) return;
                 window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
                 window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                EditText batdau = dialog.findViewById(R.id.batdau);
+                EditText ketthuc = dialog.findViewById(R.id.ketthuc);
+                DatePickerDialog.OnDateSetListener dateBD = (view1, year, month, day) -> {
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH,month);
+                    myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                    batdau.setText(formatDay.format(myCalendar.getTime()));
+                };
+                DatePickerDialog.OnDateSetListener dateKT = (view1, year, month, day) -> {
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH,month);
+                    myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                    ketthuc.setText(formatDay.format(myCalendar.getTime()));
+                };
+                batdau.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new DatePickerDialog(QLVoucherActivity.this, dateBD, myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                    }
+                });
+                ketthuc.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new DatePickerDialog(QLVoucherActivity.this, dateKT, myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                    }
+                });
+
                 CardView cancel = dialog.findViewById(R.id.cancel_button);
                 cancel.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -58,18 +140,78 @@ public class QLVoucherActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
+                EditText discount = dialog.findViewById(R.id.discount);
+                EditText sl = dialog.findViewById(R.id.sl);
+                CardView confirm = dialog.findViewById(R.id.add_button);
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (Integer.valueOf(discount.getText().toString().trim()) <= 0
+                                || Integer.valueOf(discount.getText().toString().trim()) > 100)
+                        {
+                            Toast.makeText(QLVoucherActivity.this, "Discount phải <= 100 và > 0", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        try {
+                            if (formatDay.parse(batdau.getText().toString())
+                                    .after(formatDay.parse(ketthuc.getText().toString())))
+                            {
+                                Toast.makeText(QLVoucherActivity.this, "Ngày bắt đầu phải < ngày kết thúc", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("giaTri",Integer.valueOf(discount.getText().toString().trim()) );
+                        data.put("soLuong", Integer.valueOf(sl.getText().toString().trim()) );
+                        data.put("ngayBD", batdau.getText().toString());
+                        data.put("ngayKT", ketthuc.getText().toString());
+                        data.put("is_deleted", false);
+                        db.collection("Voucher").add(data);
+                        dialog.dismiss();
+                        renderList();
+                    }
+                });
+
                 dialog.show();
             }
         });
     }
 
-    private ArrayList<vouCher> getListVoucher() {
-        ArrayList<vouCher> l = new ArrayList<>();
-        l.add(new vouCher("123", "2/9/1945", "30/4/1975",23, 50));
-        l.add(new vouCher("123", "2/9/1945", "30/4/1975",23, 20));
-        l.add(new vouCher("123", "2/9/1945", "30/4/1975",23, 50));
-        l.add(new vouCher("123", "2/9/1945", "30/4/1975",23, 75));
-        return l;
+    private void renderList() {
+        ListVoucherSDR.clear();
+        ListVoucherDDR.clear();
+        db.collection("Voucher")
+                .whereEqualTo("is_deleted", false)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            try {
+                                if (formatDay.parse(document.getString("ngayKT")).after(Calendar.getInstance().getTime())){
+                                    vouCher vouCher = new vouCher(
+                                            document.getId(),
+                                            document.getString("ngayBD"),
+                                            document.getString("ngayKT"),
+                                            document.getDouble("soLuong").intValue(),
+                                            document.getDouble("giaTri").intValue()
+                                    );
+                                    if (formatDay.parse(document.getString("ngayBD")).before(Calendar.getInstance().getTime()))
+                                        ListVoucherDDR.add(vouCher);
+                                    else
+                                        ListVoucherSDR.add(vouCher);
+                                }
+                            } catch (ParseException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(this, "Can't get data", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/example/damb_qlnh/adapter/VoucherAdapter.java
+++ app/src/main/java/com/example/damb_qlnh/adapter/VoucherAdapter.java
@@ -1,14 +1,21 @@
 package com.example.damb_qlnh.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import com.example.damb_qlnh.R;
 import com.example.damb_qlnh.models.vouCher;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -19,6 +26,11 @@ public class VoucherAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<vouCher> list;
     private int layout;
+
+    public void setList(ArrayList<vouCher> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
 
     public VoucherAdapter(Context context, int layout, ArrayList<vouCher> list) {
         this.context = context;
@@ -43,6 +55,7 @@ public class VoucherAdapter extends BaseAdapter {
     private class ViewHolder {
         TextView discount;
         TextView mota;
+        ImageView xoa;
     }
 
     @Override
@@ -55,6 +68,7 @@ public class VoucherAdapter extends BaseAdapter {
             viewHolder = new ViewHolder();
             viewHolder.discount = view.findViewById(R.id.discount);
             viewHolder.mota = view.findViewById(R.id.mota);
+            viewHolder.xoa = view.findViewById(R.id.delete_btn);
             view.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) view.getTag();
@@ -62,8 +76,41 @@ public class VoucherAdapter extends BaseAdapter {
         viewHolder.discount.setText("Discount " + vc.getGiaTri() + "%");
         String strBD = vc.getNgayBD();
         String strKT = vc.getNgayKT();
-        String strMota = "Thời gian: " + strBD + "-" + strKT + "\nSố lượng còn lại: " + vc.getSoLuong();
+        String strMota = "Thời gian: " + strBD + " – " + strKT + "\nSố lượng còn lại: " + vc.getSoLuong();
         viewHolder.mota.setText(strMota);
+        viewHolder.xoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Xóa voucher");
+                builder.setMessage("Bạn có đồng ý xóa voucher ?");
+                builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                        DocumentReference documentRef = firestore.collection("Voucher").document(vc.getMaVoucher());
+
+                        documentRef.update("is_deleted", true)
+                                .addOnSuccessListener(aVoid -> {
+                                    list.remove(i);
+                                    notifyDataSetChanged();
+                                    Toast.makeText(context, "Xóa thành công", Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(context, "Thất bại", Toast.LENGTH_SHORT).show();
+                                });
+                    }
+                });
+                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
         return view;
     }
 }
